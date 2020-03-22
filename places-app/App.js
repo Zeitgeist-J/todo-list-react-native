d1de0884c461de6b221906765c65fb4746b65717
Nicolas Macias <nicolas.macias@creci.co>
2020-03-22 17:13:55 -0500

    adding and deleting elements

--- places-app/App.js
+++ places-app/App.js
@@ -1,21 +1,49 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  FlatList
+} from 'react-native';
+
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
-  const [outputText, setOutputText] = useState('Open up App.js to start working on your app!')
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const addGoalHandler = goalTitle => {
+    setCourseGoals(currentGoals => [...courseGoals, {
+      id: Math.random().toString(),
+      value: goalTitle
+    }]);
+  }
+
+  const removeGoalHandler = goalId => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter((goal) => goal.id !== goalId);
+    });
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button title="Change text" onPress={() => setOutputText('The text has changed!')}/>
+    <View style={styles.screen}>
+      <GoalInput onAddGoal={addGoalHandler} />
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={courseGoals}
+        renderItem={itemData =>
+          <GoalItem
+            id={itemData.item.id}
+            onDelete={removeGoalHandler}
+            value={itemData.item.value}
+          />
+        }
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
 });
